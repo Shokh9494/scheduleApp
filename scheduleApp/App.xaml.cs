3c9c2663e45b64448eb59452e958e795c68ea430
wox9494 <wox1686482@gmail.com>
2022-11-21 14:59:45 +0500

    Add project files.

---
+++ scheduleApp/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace scheduleApp;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ scheduleApp/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace scheduleApp;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
